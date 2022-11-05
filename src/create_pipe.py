8947e29e1c14de28d9f8d6dc661d737239c9f13a
Marta Osińska <martasnsk@gmail.com>
2022-11-05 16:21:48 +0100

    ✨ added group class

--- src/create_pipe.py
+++ src/create_pipe.py
@@ -1,6 +1,5 @@
 from clean_text import clean_text
 from nltk.corpus import wordnet
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neighbors import NearestNeighbors
 from sklearn.base import BaseEstimator, TransformerMixin
 import pandas as pd
@@ -15,14 +14,15 @@ nltk.download('averaged_perceptron_tagger')
 with zipfile.ZipFile('.\data\glove\glove.6B.50d.zip', 'r') as zip_ref:
     zip_ref.extractall('.\data\glove')
 
+
 def embedding_for_vocab(filepath, word_index,
                         embedding_dim):
     vocab_size = len(word_index) + 1
-      
+
     # Adding again 1 because of reserved 0 index
     embedding_matrix_vocab = np.zeros((vocab_size,
                                        embedding_dim))
-  
+
     with open(filepath, encoding="utf8") as f:
         for line in f:
             word, *vector = line.split()
@@ -30,8 +30,9 @@ def embedding_for_vocab(filepath, word_index,
                 idx = word_index[word]
                 embedding_matrix_vocab[idx] = np.array(
                     vector, dtype=np.float32)[:embedding_dim]
-  
+
     return embedding_matrix_vocab
+
 
 class CleanText(BaseEstimator, TransformerMixin):
     def __init__(self, columns=None):
@@ -45,6 +46,24 @@ class CleanText(BaseEstimator, TransformerMixin):
         return X
 
 
+class Group(BaseEstimator, TransformerMixin):
+    def __init__(self, columns=None):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        X = X[self.columns]
+        X = X.groupby(
+            self.columns[0]).agg(
+                list).reset_index()
+        X[self.columns[1]] = X[self.columns[1]].apply(
+            lambda x: ' '.join(x))
+        X = X[self.columns[1]].tolist()
+        return X
+
+
 class Tokenize(BaseEstimator, TransformerMixin):
     def __init__(self, columns=None):
         self.columns = columns
@@ -55,8 +74,9 @@ class Tokenize(BaseEstimator, TransformerMixin):
     def transform(self, X, y=None):
         all_lyrics = ' '.join(list(X[self.columns]))
         vocab = list(set(nltk.word_tokenize(all_lyrics)))
-        word_index = {vocab[i]:i for i in range(len(vocab))}
+        word_index = {vocab[i]: i for i in range(len(vocab))}
         return word_index
+
 
 class Embed(BaseEstimator, TransformerMixin):
     def __init__(self):
@@ -67,6 +87,5 @@ class Embed(BaseEstimator, TransformerMixin):
 
     def transform(self, X, y=None):
         embedding_matrix_vocab = embedding_for_vocab(
-            '.\data\glove\glove.6B.50d.txt', X,50)
+            '.\data\glove\glove.6B.50d.txt', X, 50)
         return embedding_matrix_vocab
-
