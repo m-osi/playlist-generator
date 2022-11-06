b9ea6c2651c3ab97fd47861422bcce6086facb6f
Marta Osińska <martasnsk@gmail.com>
2022-11-06 22:34:36 +0100

    ✨ get features & return results

--- src/app.py
+++ src/app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from predict import predict
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template(
+        "index.html")
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    features = [
+        request.form['dance'], 
+        request.form['cheer'], 
+        request.form['acoustic'], 
+        10]
+    features = [(int(x)/10) for x in features]
+    try:
+        playlist = predict(request.form['lyrics'], features)
+        return render_template(
+        "results.html", playlist=playlist)
+    except Exception as e:
+        message = f'Error: {e}. Please try again!'
+        return message
+    
 
 if __name__ == "__main__":
     app.run()
